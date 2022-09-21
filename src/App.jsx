5847d81be6858d752c2553414d632cb15d39eede
AMD <michojekunle1@gmail.com>
2022-09-21 08:52:35 +0100

    done with context

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
+import Header from './components/Header';
 import ThemeContextProvider from './context/ThemeContext';
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        APP
+        <Header />
       </ThemeContextProvider>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ThemeContext } from '../context/ThemeContext'
 
 const Header = () => {
+    const { theme, toggleTheme } = useContext(ThemeContext);
   return (
-    <div>
-      
-    </div>
+    <header role={"menubar"} style={{backgroundColor: theme.bg}}>
+      <h1>Where in the world?</h1>
+      <button onClick={toggleTheme} type='button'><i className="fas fa-moon"></i>Dark Mode</button>
+    </header>
   )
 }
 
