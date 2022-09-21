6192a4c7693ef515299b92661f65bf237deeba8e
AMD <michojekunle1@gmail.com>
2022-09-21 09:46:01 +0100

    done with header and toggle theme effect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,11 @@ const Header = () => {
     const { theme, toggleTheme } = useContext(ThemeContext);
   return (
     <header role={"menubar"} style={{backgroundColor: theme.bg}}>
-      <h1>Where in the world?</h1>
-      <button onClick={toggleTheme} type='button'><i className="fas fa-moon"></i>Dark Mode</button>
+      <h1 style={{color: theme.text}}>Where in the world?</h1>
+      <button style={{color: theme.text}} onClick={toggleTheme} type='button'>
+        <i className="fas fa-moon"></i>
+        Dark Mode
+      </button>
     </header>
   )
 }
