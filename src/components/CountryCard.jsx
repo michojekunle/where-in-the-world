e5d8925383f2e8c5e7ea98aa718bb199a41cf5ac
AMD <michojekunle1@gmail.com>
2022-09-22 06:45:05 +0100

    good morning

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { ThemeContext } from '../context/ThemeContext';
 
 const CountryCard = ({country}) => {
   const { flags: { png }, name: { official }, population, region, capital } = country;
- 
+  const { theme } = useContext(ThemeContext);
   return (
-    <div className='country-card'>
+    <div className='country-card' style={{background: theme.elements}}>
       <div className="flag"><img src={png} alt={official} /></div>
-      <h3>{official}</h3>
-      <p>Population: <span>{population}</span></p>
-      <p>Region: <span>{region}</span></p>
-      <p>Capital: <span>{capital?.map(cap => cap)}</span></p>
+      <div className="about-country">
+        <h3 style={{color: theme.text}}>{official}</h3>
+        <p style={{color: theme.text}}>Population: <span>{population}</span></p>
+        <p style={{color: theme.text}}>Region: <span>{region}</span></p>
+        <p style={{color: theme.text}}>Capital: <span>{capital?.map(cap => cap)}</span></p>
+      
+      </div>
     </div>
   )
 }
