6c437770482892a1cff02da053b0bf3b14aa2851
AMD <90222287+michojekunle@users.noreply.github.com>
2022-12-24 15:53:09 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext } from 'react';
 import { CountriesContext } from '../context/CountriesContext';
 import { ThemeContext } from '../context/ThemeContext';
 
